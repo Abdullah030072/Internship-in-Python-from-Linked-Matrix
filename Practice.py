324c1c063684891c3e5d81eb39e81f1dde82ef4e
Abdullah030072 <abdullah030072@gmail.com>
2026-07-08 15:19:40 +0500

    practice

---
+++ Practice.py
@@ -0,0 +1,66 @@
+num = 100
+print (type(num))
+text ="ABDULLAH ABD"
+print (text[1])
+print(text[1:3])
+print(text[::-1])
+print(text.lower())
+print(text.find("H"))
+print(text.replace("ABD","ABC"))
+print(text.split())
+print(text.title())
+print(text.capitalize())
+print(text.upper())
+print(len(text))
+numbers = (10,20,30)
+print(numbers)
+print(numbers[1])
+#This is comment;
+#single word must be write in single or double commas
+x='john'
+print(x)
+y="john"
+print(y)
+Age=39
+print(Age)
+age=35
+print(age)
+#assign the diferent valuse to different variables
+x,y,z="Oraange","Banana","Cheery"
+print (x)
+print (y)
+print (z)
+#assign the same value to different variables
+x =y =z ="BANANA"
+print (x)
+print (y)
+print (z)
+#unpack a collection
+fruits = ["orange","banana","apple"]
+x,y,z= fruits
+print(x)
+print(y)
+print(z)
+#function
+x="awesome"
+def myfun():
+    print("Python is "+x)
+myfun()
+#global variable
+def abc():
+    global a
+    a ="fantastic"
+abc()
+print ("python is "+a)
+#python output variable 
+#also  use the sign of + instead of comma 
+ #but in + dont space added
+#you dont add string into number  
+#you show the string and number with the help of comma.  
+q = "abc"
+w = 'axbd'
+e = 'buiv'
+print (q,w,e)
+print (q+w+e)  
+ 
+ 
